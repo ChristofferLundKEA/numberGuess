c7bd206fbce991c603b3e251836dfa650057670c
Christoffer Lund <Christofferlund93@gmail.com>
2025-02-19 00:05:07 +0100

    fin

--- 7-DOM-2/guess-my-number/game.js
+++ 7-DOM-2/guess-my-number/game.js
@@ -1,57 +1,74 @@
-//Save the buttons as constants
 const guessBtn = document.querySelector('.guessBtn');
-let userInputField = 0;
 const paragraph = document.querySelector('.paragraph');
 const higherLowerParagraph = document.querySelector('.higher-lower-paragraph');
 const resetBtn = document.querySelector('#reset')
 const highscoreParagraph = document.querySelector('#highscore')
+const userGuess = document.querySelector('#userGuess')
+
+let userInputField = 0;
 let guessTries = 0;
 let savedHighscore = 0;
 let liveHighscore = 0;
 
-//Add an eventListener that takes input from the input field
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter")  guessBtn.click();
+    if (event.key === "Escape") resetBtn.click();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#userGuess").focus();
+});
+
 guessBtn.addEventListener("click", () => {
 
-    userInputField = parseInt(document.querySelector('#userGuess').value);
+    userGuess.focus();
+    userInputField = parseInt(userGuess.value);
     guessTries++;
 
     userInputField !== randomNumber
-        ? paragraph.innerText = 'You were totally wrong'
+        ? paragraph.innerText = `${userInputField} is totally wrong`
         : paragraph.innerText = "You are right!";
 
     if (userInputField !== randomNumber){
+        userGuess.value = "";
+        higherLowerParagraph.style.display = "block";
         userInputField < randomNumber
             ? higherLowerParagraph.innerText = "go higher"
             : higherLowerParagraph.innerText = "go lower";
         liveHighscore = 0; //fix her, ved ikke om det overhoved er rigtigt
     }
 
-    // if (userInputField === randomNumber) highscoreParagraph.hide(); fix paa et tidspunkt
     if (userInputField === randomNumber) {
         randomNumber = generateNumber();
+        higherLowerParagraph.style.display = "none";
+        userGuess.value = "";
         console.log(randomNumber);
 
         if (guessTries === 1) {
             liveHighscore++;
-
             if (liveHighscore > savedHighscore) {
                 savedHighscore = liveHighscore;
                 highscoreParagraph.innerText = liveHighscore;
             }
-            guessTries = 0;
-
         }
+        guessTries = 0;
+    }
+
+    if (isNaN(userInputField)) {
+        paragraph.innerText = "Are you trying to crash me?\nJust type a number between 1 & 5 :)";
+        higherLowerParagraph.style.display = "none";
     }
 });
 
 resetBtn.addEventListener('click', () => {
+    userGuess.value = "";
+    userGuess.focus();
     randomNumber = generateNumber();
     paragraph.innerText = '';
     higherLowerParagraph.innerText = '';
     savedHighscore = 0;
-    highscoreParagraph.innerText = ''
+    highscoreParagraph.innerText = 0;
 })
-//Print something to the console
 
 //Make a random number between 1-5
 let randomNumber = generateNumber();
